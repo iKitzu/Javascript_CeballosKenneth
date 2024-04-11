5c93076c309e13ab249061602bc8dd7d6a247576
iKitzu <kennethsantiagoc@gmail.com>
2024-04-11 08:34:46 -0500

    docs: :wrench: Script

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-a=3
-b=2
+// ** FUNCION SIN RETORNO Y SIN PARAMETROS **
+function funcionNormal(){
+    console.log("Mi función");
+}
 
-console.log(a*b)
-console.log(a/b)
-console.log(a+b)
-console.log(a-b)
-console.log(a)
-console.log(b)
+// ** FUNCION SIN RETORNO Y CON PARAMETROS **
+function suma(a,b){
+    console.log(a+b);
+}
+// ** FUNCION CON RETORNO Y CON PARAMETROS **
+function sumaR(a,b){
+    //console.log(a+b);
+    return a+b;
+}
+// ** FUNCION CON RETORNO Y SIN PARAMETROS **
+function salonFavorito(){
+    //console.log(a+b);
+    return "P1";
+}
+
+//*****Bucle For****/
+
+for(let i=0;i<10;i=i+1){
+    console.log(i);
+}
