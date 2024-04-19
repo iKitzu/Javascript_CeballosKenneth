288202bddf60603c1b6cd0fbba2331ad5e416044
iKitzu <kennethsantiagoc@gmail.com>
2024-04-19 06:41:18 -0500

    feat: :tada: Desarrollo primer punto de la tarea de JS Classroom

---
+++ Dia 5/script.js
@@ -0,0 +1,24 @@
+console.log("======= Ejercicio 1 =======");
+
+class persona {
+    constructor (nombre, edad, pais) {
+        this.nombre=nombre
+        this.edad=edad
+        this.pais=pais
+    }
+
+    mostrardetalles() {
+        console.log (`Nombre: ${this.nombre}, Edad: ${this.edad}, País: ${this.pais}`);
+    }
+}
+
+const persona1 = new persona ('Juan','30', 'Colombia');
+const persona2 = new persona ('Pedro','22', 'Burgundoforilandia');
+
+console.log ("\nDetalles de la Persona 1:");
+persona1.mostrardetalles();
+
+console.log("\nDetalles de la persona 2:");
+persona2.mostrardetalles();
+
+console.log("======= Ejercicio 2 =======");
