ce7df81f89bb1e5ded59494cf553b3b6ed6885cb
iKitzu <kennethsantiagoc@gmail.com>
2024-04-20 10:11:03 -0500

    refactor: :sparkles:

---
+++ Dia 0/script.js
@@ -0,0 +1,25 @@
+// ** FUNCION SIN RETORNO Y SIN PARAMETROS **
+function funcionNormal(){
+    console.log("Mi función");
+}
+
+// ** FUNCION SIN RETORNO Y CON PARAMETROS **
+function suma(a,b){
+    console.log(a+b);
+}
+// ** FUNCION CON RETORNO Y CON PARAMETROS **
+function sumaR(a,b){
+    //console.log(a+b);
+    return a+b;
+}
+// ** FUNCION CON RETORNO Y SIN PARAMETROS **
+function salonFavorito(){
+    //console.log(a+b);
+    return "P1";
+}
+
+//*****Bucle For****/
+
+for(let i=0;i<10;i=i+1){
+    console.log(i);
+}
