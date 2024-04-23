7e3732bac2b5bd829aa247e8d5069aaf7580b660
iKitzu <kennethsantiagoc@gmail.com>
2024-04-23 08:57:11 -0500

    docs: :poop: Datos solo en consola (a medias)

--- Dia 8/script.js
+++ Dia 8/script.js
@@ -1,5 +1,43 @@
-const results = document.querySelector('#results');
+fetchData();
 
-document.querySelector('#buttons').addEventListener('click', e => {
-    asyncFetch(e.target.textContent.trim().toLowerCase())
-})
+async function fetchData() {
+
+    try {
+        const response = await fetch("https://swapi.py4e.com/api/people/1")
+        
+        if(!response.ok){
+            throw new Error("No se pudo encontrar");
+        }
+    
+        const data = await response.json();
+        console.log(data);
+    }
+    
+    catch(error){
+        console.error(error);
+    }
+
+}
+
+
+// async function fetchData() {
+// 
+    // try {
+// 
+//         const stwname = document.getElementById("stwname").value.toLowerCase();
+// 
+//         const response = await fetch(`https://swapi.py4e.com/api/${stwname}`)
+//         
+//         if(!response.ok){
+//             throw new Error("No se pudo encontrar");
+//         }
+//     
+//         const data = await response.json();
+//         console.log(data);
+//     }
+//     
+//     catch(error){
+//         console.error(error);
+//     }
+// 
+// }
