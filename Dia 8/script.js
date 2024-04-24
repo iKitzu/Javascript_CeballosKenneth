95d7a25624bc17d9b8350225f79bb45044185400
iKitzu <kennethsantiagoc@gmail.com>
2024-04-24 14:26:32 -0500

    feat: :construction: Llegue hasta la correccion del profesor
    A partir de aqui empieza mi trascendencia a la locura para terminar lo que empezo

--- Dia 8/script.js
+++ Dia 8/script.js
@@ -1,43 +1,55 @@
-fetchData();
-
-async function fetchData() {
-
-    try {
-        const response = await fetch("https://swapi.py4e.com/api/people/1")
-        
-        if(!response.ok){
-            throw new Error("No se pudo encontrar");
+function fetchStar() {
+    let xhr = new XMLHttpRequest();
+    let url = 'https://swapi.py4e.com/api/people/1';
+    xhr.open('GET', url, true);
+    xhr.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            let response = JSON.parse(this.responseText);
+            console.log(response)
+            displayStar(response);
         }
-    
-        const data = await response.json();
-        console.log(data);
+        else {
+            console.log("Error! >:c");
+            displayStar(this.statusText);
+        }
     }
-    
-    catch(error){
-        console.error(error);
+    xhr.send();
+}
+
+function displayStar(data) {
+    let starinfo = document.getElementById('losdatos');
+    if (data.response === "error") {
+        starinfo.innerHTML = '<p> ERROR </p>'
+    } else {
+        console.log(data);
+        starinfo.innerHTML= `
+        <table class="table table">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Height</th>
+            <th>Mass</th>
+            <th>Hair Color</th>
+            <th>Skin Color</th>
+            <th>Birth Year</th>
+            <th>Gender</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>${data.name}</td>
+            <td>${data.height}</td>
+            <td>${data.mass}</td>
+            <td>${data.hair_color}</td>
+            <td>${data.skin_color}</td>
+            <td>${data.birth_year}</td>
+            <td>${data.gender}</td>
+          </tr>
+        </tbody>
+      </table>
+      `
     }
 
 }
 
-
-// async function fetchData() {
-// 
-    // try {
-// 
-//         const stwname = document.getElementById("stwname").value.toLowerCase();
-// 
-//         const response = await fetch(`https://swapi.py4e.com/api/${stwname}`)
-//         
-//         if(!response.ok){
-//             throw new Error("No se pudo encontrar");
-//         }
-//     
-//         const data = await response.json();
-//         console.log(data);
-//     }
-//     
-//     catch(error){
-//         console.error(error);
-//     }
-// 
-// }
+fetchStar();
